95455c0244ee4019b531237c973b8fd0d1240f51
trungnm14498 <trungnm.14498@gmail.com>
2022-06-09 12:04:21 +0300

    Day 15

---
+++ Day 15/README.md
@@ -0,0 +1,4 @@
+# MockData
+- Sử dụng API lần thứ hai, làm quen với fetch
+- innerHTML là thay thế toàn bộ chứ không phải thêm vào như trong file README về DOM (f8) mà mình đã tự viết -> đã sửa
+- Nói chung là cần thực hành nhiều để có thể ứng dụng những cái đã học 
---
+++ Day 15/app.js
@@ -0,0 +1,36 @@
+fetch('http://fakestoreapi.com/products')
+.then(res=> {
+    return res.json()
+})
+.then(data=>{
+    var products = document.querySelector('.products');
+
+    products.innerHTML = '';
+    data.forEach(item => {
+        var newProduct = document.createElement('div');
+        newProduct.classList.add('product');
+        newProduct.innerHTML = `
+            <img src="${item.image}" alt="">
+            <div class="info">
+                <div class="name">${item.title}</div>
+                <div class="price">${item.price}</div>
+            </div>
+        `;
+        products.appendChild(newProduct);
+    })
+})
+
+var searchInput = document.querySelector('.search input');
+
+searchInput.addEventListener('input', function(e){
+     let txtSearch = e.target.value.trim().toLowerCase();
+     let listProductDOM = document.querySelectorAll('.product')
+     listProductDOM.forEach(item=>{
+        if(item.innerText.toLowerCase().includes(txtSearch)) {
+            item.classList.remove('hide');
+        } else {
+            item.classList.add('hide');
+        }
+     })
+})
+
